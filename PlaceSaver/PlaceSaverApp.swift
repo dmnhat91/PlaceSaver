81eb2edb98ef3d0cb2863986507ff2a4b13ef44a
dmnhat91 <henry.duong02@gmail.com>
2022-08-04 21:45:46 +0700

    Initial Commit

---
+++ PlaceSaver/PlaceSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceSaverApp.swift
+//  PlaceSaver
+//
+//  Created by Duong Minh Nhat on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PlaceSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
