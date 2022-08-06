d32764d291c34aaa7470fb4a50c63974887a47d4
dmnhat91 <henry.duong02@gmail.com>
2022-08-06 21:07:20 +0700

    Create welcome screen

---
+++ PlaceSaver/Models/ColorConstants.swift
@@ -0,0 +1,14 @@
+//
+//  ColorConstants.swift
+//  PlaceSaver
+//
+//  Created by Duong Minh Nhat on 06/08/2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct ColorConstants {
+    static let lightGreen = Color(red: 0.89, green: 1, blue: 0.89)
+    static let heavyGreen = Color(red: 0, green: 0.588, blue: 0)
+}
---
+++ PlaceSaver/Views/Welcome/GreetingView.swift
@@ -0,0 +1,59 @@
+//
+//  GreetingView.swift
+//  PlaceSaver
+//
+//  Created by Duong Minh Nhat on 06/08/2022.
+//
+
+import SwiftUI
+
+struct GreetingView: View {
+    @Binding var active: Bool
+    
+    var body: some View {
+        ZStack {
+            ColorConstants.lightGreen
+                .ignoresSafeArea(.all, edges: .all)
+            
+            VStack {
+                Spacer()
+                
+                Image("icon-symbol")
+                Text("Welcome")
+                  .font(.system(size: 50))
+                  .fontWeight(.heavy)
+                  .padding(.bottom)
+                Text("""
+                    Save your favorite locations.
+                    Eliminate the need for memorization!
+                    """)
+                  .font(.title3)
+                  .fontWeight(.bold)
+                  .multilineTextAlignment(.center)
+                  .padding(.horizontal, 10)
+                
+                Spacer()
+                
+                Button(action: {
+//                    active = false
+                }, label: {
+                    Capsule()
+                      .fill(ColorConstants.heavyGreen)
+                      .padding(8)
+                      .frame(height:80)
+                      .overlay(Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white))
+                })
+                
+            }
+        }
+    }
+}
+
+struct GreetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        GreetingView(active: .constant(true))
+    }
+}
